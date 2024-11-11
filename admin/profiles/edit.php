79dda6832723ba61d322ae370b600f9b3dda9196
UTakuto <taku.0307.ue@icloud.com>
2024-11-11 14:33:51 +0900

    プロフィール表示 , 変更

--- admin/profiles/edit.php
+++ admin/profiles/edit.php
@@ -58,28 +58,28 @@ var_dump($profile , $jobs);
             <form action="store.php" method="POST" class="max-w-sm mx-auto">
                 <div class="pt-[8px]">
                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900">NAME.</label>
-                    <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    <input type="text" name="name" id="name" value="<?= $profile -> name?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                 </div>
                 <div class="pt-[8px]">
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900">EMAIL.</label>
-                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    <input type="email" name="email" id="email" value="<?= $profile -> email?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                 </div>
                 <div class="pt-[8px]">
                     <label for="job1" class="block mb-2 text-sm font-medium text-gray-900">JOB1.</label>
-                    <input type="text" name="job1" id="job1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    <input type="text" name="job1" id="job1" value="<?= ( empty($jobs[0]))?"" : $jobs[0] ->name ?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                 </div>
                 <div class="pt-[8px]">
                     <label for="job2" class="block mb-2 text-sm font-medium text-gray-900">JOB2.</label>
-                    <input type="text" name="job2" id="job2" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    <input type="text" name="job2" id="job2" value="<?= ( empty($jobs[1]))?"" : $jobs[1] ->name ?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                 </div>
                 <div class="pt-[8px]">
                     <label for="about" class="block mb-2 text-sm font-medium text-gray-900">ABOUT.</label>
-                    <textarea name="about" id="about" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
+                    <textarea name="about" id="about" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"><?= $profile -> about ?></textarea>
                 </div> 
                 <div class="flex justify-center">
-                    <button type="submit" class="inline-block rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                    <!-- <button type="submit" class="inline-block rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
                         再登録
-                    </button>
+                    </button> -->
                 </div>
             </form>
 
