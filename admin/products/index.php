31410ec4c6fe7f8b375c9d91135f48bbb51eb0d5
UTakuto <taku.0307.ue@icloud.com>
2024-11-18 14:34:19 +0900

    管理画面用のプロフィール一覧表示機能を追加

--- admin/products/index.php
+++ admin/products/index.php
@@ -38,6 +38,33 @@ catch(Exception $error){
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    
+    <div class="w-full m-auto">
+        <?php require __DIR__ . "/../header.php"; ?>
+        <main class="p-10 flex flex-row flex-wrap items-center justify-center">
+        <?php foreach($profiles as $profile) : ?>
+            <div class="border-solid border-[1px] border-slate-500 w-[500px] h-[200px] flex items-center m-[10px] rounded overflow-hidden shadow-md bg-white">
+                <div class="px-6 py-4">
+                    <div class="text-[20px] font-bold"><?= $profile -> name ?></div>
+                    <p class="text-slate-600 leading-normal font-light">
+                        <?= $profile -> email ?>
+                    </p>
+                    <p class="text-slate-600 leading-normal font-light">
+                        <?= $profile -> about ?>
+                    </p>
+                    <a href="./detail.php?id=<?= $profile -> id ?>" class="inline-block rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        DETAIL
+                    </a>
+                </div>
+            </div>
+        <?php endforeach ?>
+    </main>
+    <div class="flex justify-center align-center">
+        <a href="create.php" class="inline-block rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+            CREATE
+        </a>
+    </div>
+
+    </div>
 </body>
+
 </html>
