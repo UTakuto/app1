1d4aaa475da3072514b744be2ddacccdafbee9a5
UTakuto <taku.0307.ue@icloud.com>
2024-11-18 14:38:37 +0900

    管理画面用の製品一覧表示機能を実装し、表示内容を更新

--- admin/products/index.php
+++ admin/products/index.php
@@ -41,19 +41,34 @@ catch(Exception $error){
     <div class="w-full m-auto">
         <?php require __DIR__ . "/../header.php"; ?>
         <main class="p-10 flex flex-row flex-wrap items-center justify-center">
-        <?php foreach($profiles as $profile) : ?>
-            <div class="border-solid border-[1px] border-slate-500 w-[500px] h-[200px] flex items-center m-[10px] rounded overflow-hidden shadow-md bg-white">
+        <?php foreach($products as $product) : ?>
+            <div class="border-solid border-[1px] border-slate-500 w-[500px] h-[300px] flex items-center m-[10px] rounded overflow-hidden shadow-md bg-white">
                 <div class="px-6 py-4">
-                    <div class="text-[20px] font-bold"><?= $profile -> name ?></div>
+                    <div class="text-[20px] font-bold"><?= $product -> title ?></div>
                     <p class="text-slate-600 leading-normal font-light">
-                        <?= $profile -> email ?>
+                        <?= $product -> catchcopy ?>
                     </p>
                     <p class="text-slate-600 leading-normal font-light">
-                        <?= $profile -> about ?>
+                        <?= $product -> thumbnail ?>
                     </p>
-                    <a href="./detail.php?id=<?= $profile -> id ?>" class="inline-block rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                    <p class="text-slate-600 leading-normal font-light">
+                        <?= $product -> style ?>
+                    </p>
+                    <p class="text-slate-600 leading-normal font-light">
+                        <?= $product -> grade ?>
+                    </p>
+                    <p class="text-slate-600 leading-normal font-light">
+                        <?= $product -> skill ?>
+                    </p>
+                    <p class="text-slate-600 leading-normal font-light">
+                        <?= $product -> demo ?>
+                    </p>
+                    <p class="text-slate-600 leading-normal font-light">
+                        <?= $product -> period ?>
+                    </p>
+                    <!-- <a href="./detail.php?id=<?= $product -> id ?>" class="inline-block rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
                         DETAIL
-                    </a>
+                    </a> -->
                 </div>
             </div>
         <?php endforeach ?>
