573aceca84a5bee603fba808f43ee1a7edddaec0
UTakuto <taku.0307.ue@icloud.com>
2024-11-18 16:45:19 +0900

    POST リクエストのチェックを追加し、トランザクションの有効化を実装

--- admin/products/store.php
+++ admin/products/store.php
@@ -7,4 +7,25 @@ var_dump($_POST);
 print "<hr>";
 var_dump($_FILES);
 
-?>
+// Request Method が Post かのチェック
+if( $_SERVER[ "REQUEST_METHOD" ] !== "POST" ){
+    header( "Location: create.php" );
+    exit;
+}
+
+try{
+
+    $db = new PDO( DB_DSN, DB_USER, DB_PASS );
+
+    //トランザクションの有効化
+    $db -> beginTransaction();
+
+    
+
+}
+catch( PDOException $error ){
+    print $error -> getMessage();
+}
+catch( Exception $error ){
+    print $error -> getMessage();
+}
