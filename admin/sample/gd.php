ffd758e2432b05226e6195b2888ee3c44643bda3
UTakuto <taku.0307.ue@icloud.com>
2024-12-02 15:46:35 +0900

    サムネイル画像の保存処理を追加し、メモリ解放とファイル削除を実装

--- admin/sample/gd.php
+++ admin/sample/gd.php
@@ -14,10 +14,25 @@ $screen = imagecreatefrompng($imageSrc);
 imagecopyresampled(
     $canvas, 
     $screen, 
-    0, 0, 
-    0, 0, 
-    720, 720, 
+    0, 0, //キャンバスのx , y座標
+    0, 0, //スクリーンのx , y座標
+    720, 720, //キャンバスのwidth,height
     getimagesize($imageSrc)[0], getimagesize($imageSrc)[1]
 );
 
-printf("<img src='%s'>", $imageSrc);
+//キャンバスの画像を名前をつけて保存
+//pngの透過を有効にする
+imagesavealpha($canvas ,  true);
+//image***(キャンバス , 出力先のパス , 圧縮率)
+imagepng($canvas, "../../storage/thumb.png");
+
+//メモリ内のキャンバスを解放
+imagedestroy($canvas);
+
+//出力したサムネイル画像の読み込み
+$image = file_get_contents("../../storage/thumb.png");
+
+// printf("<img src='%s'>", "data:image/png;base64," . base64_encode($image));
+
+//仮に出力したサムネイルファイルを削除
+unlink("../../storage/thumb.png");
