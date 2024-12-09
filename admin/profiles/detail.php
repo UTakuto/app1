8719ab97baeb372b9ab0505f4ec975e5bbee37b5
UTakuto <taku.0307.ue@icloud.com>
2024-12-09 15:54:35 +0900

    プロフィール詳細ページに編集リンクを追加

--- admin/profiles/detail.php
+++ admin/profiles/detail.php
@@ -112,6 +112,9 @@ if($profile -> jobs ){
                 </div>
             </div>
             <div class="flex flex-col align-center justify-around">
+                <a href="./edit.php?id=<?= $profile -> id ?>" class="inline-block rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                    EDIT
+                </a>
                 <a href="index.php"
                     class="inline-block rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
                     HOME
