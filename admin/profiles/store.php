263f371946aa1b12dd38716ceda55e87812e3df3
UTakuto <taku.0307.ue@icloud.com>
2025-01-20 15:44:46 +0900

    プロフィール作成フォームにパスワードと再入力パスワードの検証を追加し、データベースにパスワードを保存する機能を実装

--- admin/profiles/store.php
+++ admin/profiles/store.php
@@ -2,12 +2,15 @@
 // admin\profiles\store.php
 
 require "../config.php";
+var_dump($_POST);
 
 try{
 
     //送信データの取得
     $name  = filter_input(INPUT_POST, "name");
     $email = filter_input(INPUT_POST, "email" , FILTER_VALIDATE_EMAIL); 
+    $password = filter_input(INPUT_POST, "password");
+    $repassword = filter_input(INPUT_POST, "repassword");
     $job1  = filter_input(INPUT_POST, "job1");
     $job2  = filter_input(INPUT_POST, "job2");
     $about = filter_input(INPUT_POST, "about");
@@ -19,12 +22,21 @@ try{
     if(!$email){
         throw new Exception("メールアドレスが未入力です");
     }
+    if(!$password || !$repassword){
+        throw new Exception("パスワードまたは、パスワード(再入力)が未入力です");
+    }
+    if($password !== $repassword){
+        throw new Exception("パスワードが一致しません");
+    }
     if(!$job1 && !$job2){
         throw new Exception("希望職種を1つ以上選択してください");
     }
     if(!$job1){
         throw new Exception("希望職種1が未入力です");
     }
+
+    // 暗号化パスワード
+    $hashPassword = password_hash($password , PASSWORD_DEFAULT , ["cost" => 12]);
 
     // jobの配列を作る
     $jobs = [];
@@ -36,16 +48,28 @@ try{
     }
 
     $db = new PDO( DB_DSN, DB_USER, DB_PASS );
+    
     // トランザクション
     $db -> beginTransaction();
 
     $profileTable = TB_PROFILES;
     $jobsTable = "app1_jobs";
 
+    //emailの重複チェック
+    $profileSelectSql = " SELECT count(id) AS count FROM {$profileTable} WHERE email = :email ";
+    $stmt = $db -> prepare($profileSelectSql);
+    $stmt -> execute(["email" => $email]);
+    $count = $stmt -> fetchObject();
+
+    //重複していたらエラー
+    if($count -> count > 0){
+        throw new Exception("このメールアドレスは既に登録されています");
+    }
+
     //profile insert
     $profileSql ="
-        INSERT INTO {$profileTable} (name, email, about)
-        VALUES (:name, :email, :about)
+        INSERT INTO {$profileTable} (name, email, password, about)
+        VALUES (:name, :email, :password, :about)
     ";
 
     $stmt = $db -> prepare($profileSql);
@@ -53,6 +77,7 @@ try{
         [
             "name" => $name, 
             "email" => $email, 
+            "password" => $hashPassword, 
             "about" => $about,
         ]
     );
@@ -70,6 +95,8 @@ try{
     foreach($jobs as $job){
         $stmt -> execute(["name" => $job, "profile_id" => $insertID]);
     }
+
+    $db -> commit();
 }
 catch(PDOException $error){
     print $error->getMessage() . ">>>" . $stmt->queryString;
