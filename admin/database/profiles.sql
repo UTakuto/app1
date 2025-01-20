379a9165f5e8a11abee29e9755f99bb5a0ef0e34
UTakuto <taku.0307.ue@icloud.com>
2025-01-20 15:43:53 +0900

    プロフィールテーブルにパスワードカラムを追加

--- admin/database/profiles.sql
+++ admin/database/profiles.sql
@@ -8,39 +8,12 @@ CREATE TABLE app1_profiles(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY ,    # 主キー
     name VARCHAR(255) NOT NULL ,                    # 作者名
     email VARCHAR(255) NOT NULL ,                   # メールアドレス
+    password VARCHAR(255) NOT NULL ,                # パスワード 
     about TEXT ,                                    # 自己紹介
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, # 作成日時
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
                 ON UPDATE CURRENT_TIMESTAMP         # 更新日時
  ) ENGINE = INNODB;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
 
 # Test Data (seed)
 INSERT INTO app1_profiles (name, email) VALUES('Fin', 'fin@gmail.com');
